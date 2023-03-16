7c941c2e9aa562c4a4a37ca5f7899c6a7376364d
Bayan Abu Samra <abusamrabayan@gmail.com>
2023-03-16 14:48:11 +0200

    add global style

--- src/global/style.js
+++ src/global/style.js
@@ -1,27 +1,35 @@
-import styled,{ createGlobalStyle } from "styled-components";
-
+import styled, { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
+
+html{
+  scroll-behavior: smooth;
+}
 
 body {
     font-family: 'Arial';
 }
+
 ul {
     list-style: none;
 }
+
 .active {
-    color: black;
-    font-weight: 600;
-    font-size: 20px;
-    line-height: 24px;
-    text-decoration: none;
+  text-decoration: none;
+  color: black;
+  font-weight: 600;
+  font-size: 20px;
+  line-height: 24px;
 }
 `;
 
+
 export const Container = styled.div`
-  min-height: 300px;
-  width: 1200px;
-  margin: 30px auto;
+  display: flex;
+  justify-content: space-around;
   padding: 0 1%;
+  margin: 30px auto;
+  width: 1200px;
   transition: all 0.5s;
+  gap: 6px;
 `;
