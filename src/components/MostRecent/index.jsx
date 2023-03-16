e1beaf4e137cb33c44487bb682d8a4bc1750c74c
Bayan Abu Samra <abusamrabayan@gmail.com>
2023-03-16 14:43:56 +0200

    add MostRecent component and style it

---
+++ src/components/MostRecent/index.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Button, Divider } from "@mui/material";
+import CardJob from "../components/../CardJop";
+import { Text } from "./../DetailsJop";
+
+
+const MostRecent = () => {
+  const [mostJob, setMostJob] = useState([]);
+  const [count, setCount] = useState(3);
+
+
+  const handleLoadJobs = () => {
+    setCount((prevCount) => prevCount + 3);
+  };
+
+
+  useEffect(() => {(async () => {
+
+      try {
+        const res = await axios.get(
+          `${process.env.REACT_APP_CRUDS_API_URL}/jobs?_sort=id&_order=desc_page=1&_limit=${count}`
+        );
+        if (res) {
+          setMostJob(res.data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, [count]);
+
+  
+  return (
+    <>
+      <Text>
+        Browse the most recent jobs that match your skills and profile
+        description to the skills clients are looking for.
+      </Text>
+
+      <CardJob Jobdetails={mostJob} />
+      <Divider variant="fullWidth" />
+      <Button
+        onClick={handleLoadJobs}
+        variant="outlined"
+        color="success"
+        style={{ borderRadius: "6px", margin: "10px 300px" }}
+      >
+        load more
+      </Button>
+    </>
+  );
+};
+
+
+export default MostRecent;
